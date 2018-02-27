0873e95556a6c998832fda881d7fed564622bb9c
Stefan Hellkvist <hellkvist@gmail.com>
2018-02-27 08:33:30 +0100

    bumping version

--- Formula/scc.rb
+++ Formula/scc.rb
@@ -1,8 +1,8 @@
 class Scc < Formula
   desc "Stagecast controller"
   homepage "https://stagecast.io"
-  url "https://stagecast.se/media/scc-0.1.2.tar.gz"
-  sha256 "4625ca769d100707bdd7ff02d8a900b1c54d2c551d22602013ec7877668b5d71"
+  url "https://stagecast.se/media/scc-0.1.3.tar.gz"
+  sha256 "66167c327a54e52ee6d3dff269982ce2798983d75ecd643f7681a2b4b042dff9"
 
   bottle :unneeded
 
